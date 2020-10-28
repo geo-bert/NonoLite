80cb9b2765d121ed0f267620c131f325bcd9b268
Maxi Pulstorm <maxipulstorm@aon.at>
2020-10-28 15:23:04 +0100

    format java

--- src/com/nonolite/Main.java
+++ src/com/nonolite/Main.java
@@ -3,16 +3,16 @@ package com.nonolite;
 import processing.core.PApplet;
 
 public class Main extends PApplet {
-    public void settings(){
+    public static void main(String[] args) {
+        PApplet.main("com.nonolite.Main");
+    }
+    
+    public void settings() {
         size(200, 200);
     }
-
-    public void draw(){
+    
+    public void draw() {
         background(100);
         ellipse(mouseX, mouseY, 20, 20);
-    }
-
-    public static void main(String[] args){
-        PApplet.main("com.nonolite.Main");
     }
 }
