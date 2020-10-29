7e7e27a711e34f1defa9a5e6a1cfc5e1a6251bc3
Maxi Pulstorm <maxipulstorm@aon.at>
2020-10-29 16:50:22 +0100

    define Board interface methods

--- src/com/nonolite/Board.java
+++ src/com/nonolite/Board.java
@@ -1,4 +1,10 @@
 package com.nonolite;
 
 public interface Board {
+    char[][] getBoard();
+    void generateBoard();
+    void generateBoard(char[][] board);
+    String put(char symbol, int x, int y);
+    void resetBoard();
+    boolean checkBoard();
 }
