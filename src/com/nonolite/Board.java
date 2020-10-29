ca9b011a4a8072904e6e7420aa4847b474eb4d48
geo-bert <grybl.business@gmail.com>
2020-10-29 17:54:17 +0100

    update Board interface to String

--- src/com/nonolite/Board.java
+++ src/com/nonolite/Board.java
@@ -1,9 +1,9 @@
 package com.nonolite;
 
 public interface Board {
-    char[][] getBoard();
-    void generateBoard();
-    void generateBoard(char[][] board);
+    String[][] getBoard();
+    void generateBoard(int height, int width);
+    void generateBoard(String[][] board);
     String put(char symbol, int x, int y);
     void resetBoard();
     boolean checkBoard();
