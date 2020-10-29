8d7446ed6bfb81be57f4fce989bddd8f13e350f7
geo-bert <grybl.business@gmail.com>
2020-10-29 18:14:52 +0100

    change Board interface put symbol to String

--- src/com/nonolite/Board.java
+++ src/com/nonolite/Board.java
@@ -4,7 +4,7 @@ public interface Board {
     String[][] getBoard();
     void generateBoard(int height, int width);
     void generateBoard(String[][] board);
-    String put(char symbol, int x, int y);
+    String put(String symbol, int x, int y);
     void resetBoard();
     boolean checkBoard();
 }
