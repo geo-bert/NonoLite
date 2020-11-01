b4647c606758993b11c4acd29e0a9c099fa93d6e
Maxi Pulstorm <maxipulstorm@aon.at>
2020-11-01 20:04:15 +0100

    change interface

--- src/com/nonolite/Board.java
+++ src/com/nonolite/Board.java
@@ -2,8 +2,9 @@ package com.nonolite;
 
 public interface Board {
     String[][] getBoard();
+    String[] getSaveBoard();
     void generateBoard(int height, int width);
-    void generateBoard(String[][] board);
+    void loadBoard(String[] board);
     String put(String symbol, int x, int y);
     void resetBoard();
     boolean checkBoard();
