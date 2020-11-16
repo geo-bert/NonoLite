d3bcf13c02f6ba7eb65a7996982b4d102d866f31
duuwhang <maxipulstorm@aon.at>
2020-11-16 17:17:11 +0100

    implement MainLayout.java dynamic drawLayout

--- src/com/nonolite/MainLayout.java
+++ src/com/nonolite/MainLayout.java
@@ -5,10 +5,10 @@ import processing.core.PGraphics;
 public class MainLayout extends Layout {
     public MainLayout(PGraphics pg) {
         super(pg);
-        
+        /*
         SideBarLayout sideBarLayout = new SideBarLayout(_pg);
         this.addChild(sideBarLayout);
-        
+        */
         BoardLayout boardLayout = new BoardLayout(_pg);
         this.addChild(boardLayout);
     }
@@ -17,10 +17,10 @@ public class MainLayout extends Layout {
     public void drawLayout(int x, int y, int width, int height) {
         _pg.push();
         for (int i = 0; i < getChildCount(); i++) {
-            int childX = x + i * width / 4;
-            int childY = y;
+            int childX = x;
+            int childY = y + i * width / 4;
             int childWidth = width;
-            int childHeight = height;
+            int childHeight = (1 + 2 * i) * height / 4;
             
             getChildAt(i).drawLayout(childX, childY, childWidth, childHeight);
         }
