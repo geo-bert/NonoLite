ed5bd4cca97d93bbe3be6b06c4b1997581f9fe94
duuwhang <maxipulstorm@aon.at>
2020-11-16 15:15:20 +0100

    format interface

--- src/com/nonolite/Board.java
+++ src/com/nonolite/Board.java
@@ -4,12 +4,20 @@ import processing.core.PGraphics;
 
 public interface Board {
     String[][] getBoard();
+    
     String[] getSaveBoard();
+    
     void generateBoard(int height, int width);
+    
     void loadBoard(String[] board);
+    
     String keyInput(int keyCode);
+    
     String mouseInput(int keyCode, int x, int y);
+    
     void resetBoard();
+    
     boolean checkBoard();
+    
     void drawBoard(PGraphics pg, int x, int y, int width, int height);
 }
