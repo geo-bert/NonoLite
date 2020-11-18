c79dcd68b20f105dd411b4fece07031c6ef14124
duuwhang <maxipulstorm@aon.at>
2020-11-18 21:55:44 +0100

    add clickable system

--- src/com/nonolite/Main.java
+++ src/com/nonolite/Main.java
@@ -1,11 +1,14 @@
 package com.nonolite;
 
+import java.util.ArrayList;
+import java.util.List;
 import processing.core.PApplet;
 
 public class Main extends PApplet {
     private static Main main;
     private int _screenWidth = 1500;
     private int _screenHeight = 800;
+    private final List<Layout> _clickables = new ArrayList<>();
     private MainLayout _mainLayout;
     
     public static void main(String[] args) {
@@ -36,11 +39,61 @@ public class Main extends PApplet {
     }
     
     public void mousePressed() {
-        _mainLayout.getBoardLayout().mouseInput(mouseButton, mouseX, mouseY);
-        System.out.println(_mainLayout.getBoardLayout().check());
+        Layout clickedLayout = null;
+        for (Layout clickable : _clickables) {
+            if (!(mouseX >= clickable.getX() &&
+                  mouseX <= clickable.getX() + clickable.getWidth())) {
+                continue;
+            }
+            if (!(mouseY >= clickable.getY() &&
+                  mouseY <= clickable.getY() + clickable.getHeight())) {
+                continue;
+            }
+            
+            if (clickedLayout != null) {
+                if (clickedLayout.getDepth() < clickable.getDepth()) {
+                    clickedLayout = clickable;
+                }
+            }
+            else {
+                clickedLayout = clickable;
+            }
+        }
+        
+        if (clickedLayout != null) {
+            clickedLayout.mouseInput(mouseButton, mouseX, mouseY);
+        }
     }
     
     public void mouseDragged() {
         _mainLayout.getBoardLayout().mouseDragInput(mouseButton, mouseX, mouseY);
+    }
+    
+    public void applyClickable(Layout layout) {
+        for (Layout clickable : _clickables) {
+            if (layout == clickable) {
+                return;
+            }
+        }
+        
+        for (int i = 0; i < _clickables.size(); i++) {
+            if (layout.getDepth() == _clickables.get(i).getDepth()) {
+                layout.setDepth(layout.getDepth() - 1);
+                i = 0;
+            }
+        }
+        _clickables.add(layout);
+    }
+    
+    public void resignClickable(Layout layout) {
+        for (Layout clickable : _clickables) {
+            if (layout == clickable) {
+                _clickables.remove(layout);
+            }
+        }
+    }
+    
+    public MainLayout getMainLayout() {
+        return _mainLayout;
     }
 }
