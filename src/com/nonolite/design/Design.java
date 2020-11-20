95154adc77e9927c3838ab219a86639ce38380af
geo-bert <grybl.business@gmail.com>
2020-11-20 20:17:15 +0100

    update interface methods

--- src/com/nonolite/design/Design.java
+++ src/com/nonolite/design/Design.java
@@ -3,35 +3,35 @@ package com.nonolite.design;
 import processing.core.PGraphics;
 
 public interface Design {
-    void background(PGraphics pg);
+    void background();
     
-    void base(PGraphics pg, int x, int y, int width, int height);
+    void base(int x, int y, int width, int height);
     
-    void rect(PGraphics pg, int x, int y, int width, int height);
+    void rect(int x, int y, int width, int height);
     
-    void rect1(PGraphics pg, int x, int y, int width, int height);
+    void rect1(int x, int y, int width, int height);
     
-    void rect2(PGraphics pg, int x, int y, int width, int height);
+    void rect2(int x, int y, int width, int height);
     
-    void text(PGraphics pg, char text, float x, float y, float width, float height);
+    void text(char text, float x, float y, float width, float height);
     
-    void text(PGraphics pg, String text, float x, float y, float width, float height);
+    void text(String text, float x, float y, float width, float height);
     
-    void text(PGraphics pg, String[] text, float x, float y, float width, float height);
+    void text(String[] text, float x, float y, float width, float height);
     
-    void text(PGraphics pg, char text, float x, float y);
+    void text(char text, float x, float y);
     
-    void text(PGraphics pg, String text, float x, float y);
+    void text(String text, float x, float y);
     
-    void text(PGraphics pg, char text, float x, float y, float size);
+    void text(char text, float x, float y, float size);
     
-    void text(PGraphics pg, String text, float x, float y, float size);
+    void text(String text, float x, float y, float size);
     
-    void text(PGraphics pg, char text, float x, float y, float size, int align1, int align2);
+    void text(char text, float x, float y, float size, int align1, int align2);
     
-    void text(PGraphics pg, String text, float x, float y, float size, int align1, int align2);
+    void text(String text, float x, float y, float size, int align1, int align2);
     
-    void symbol1(PGraphics pg, int x, int y, int width, int height);
+    void symbol1(int x, int y, int width, int height);
     
-    void symbol2(PGraphics pg, int x, int y, int width, int height);
+    void symbol2(int x, int y, int width, int height);
 }
