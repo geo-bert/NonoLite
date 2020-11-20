9932d9104b4b547ed58aa49478f82f5d6f72e3eb
duuwhang <maxipulstorm@aon.at>
2020-11-20 19:45:01 +0100

    adjust Design interface

--- src/com/nonolite/design/Design.java
+++ src/com/nonolite/design/Design.java
@@ -1,22 +1,37 @@
 package com.nonolite.design;
 
-import processing.core.PConstants;
 import processing.core.PGraphics;
 
 public interface Design {
     void background(PGraphics pg);
     
-    void base(PGraphics pg, int x, int y, int xSize, int ySize);
+    void base(PGraphics pg, int x, int y, int width, int height);
     
-    void rect(PGraphics pg, int x, int y, int xSize, int ySize);
+    void rect(PGraphics pg, int x, int y, int width, int height);
     
-    void rect1(PGraphics pg, int x, int y, int xSize, int ySize);
+    void rect1(PGraphics pg, int x, int y, int width, int height);
     
-    void rect2(PGraphics pg, int x, int y, int xSize, int ySize);
+    void rect2(PGraphics pg, int x, int y, int width, int height);
+    
+    void text(PGraphics pg, char text, float x, float y, float width, float height);
+    
+    void text(PGraphics pg, String text, float x, float y, float width, float height);
+    
+    void text(PGraphics pg, String[] text, float x, float y, float width, float height);
+    
+    void text(PGraphics pg, char text, float x, float y);
+    
+    void text(PGraphics pg, String text, float x, float y);
+    
+    void text(PGraphics pg, char text, float x, float y, float size);
+    
+    void text(PGraphics pg, String text, float x, float y, float size);
+    
+    void text(PGraphics pg, char text, float x, float y, float size, int align1, int align2);
     
     void text(PGraphics pg, String text, float x, float y, float size, int align1, int align2);
     
-    void leftClick(PGraphics pg, int x, int y, int xSize, int ySize);
+    void symbol1(PGraphics pg, int x, int y, int width, int height);
     
-    void rightClick(PGraphics pg, int x, int y, int xSize, int ySize);
+    void symbol2(PGraphics pg, int x, int y, int width, int height);
 }
