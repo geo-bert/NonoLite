1112c7d7c50e19ef8dfcd6b0ecf8246f00b53020
geo-bert <grybl.business@gmail.com>
2020-11-24 22:16:34 +0100

    implement properly

--- src/com/nonolite/layouts/utils/Toast.java
+++ src/com/nonolite/layouts/utils/Toast.java
@@ -1,6 +1,7 @@
 package com.nonolite.layouts.utils;
 
 import com.nonolite.Main;
+import com.nonolite.layouts.MainLayout;
 import processing.core.PGraphics;
 
 public class Toast extends Layout {
@@ -13,18 +14,45 @@ public class Toast extends Layout {
     
     @Override
     public void onLayout(float x, float y, float width, float height) {
+        MainLayout mainLayout = Main.getInstance().getMainLayout();
+        Rect r = new Rect();
+        r.x = mainLayout.getBoardLayout().getX();
+        r.y = mainLayout.getBoardLayout().getY();
+        r.width = mainLayout.getBoardLayout().getWidth();
+        r.height = mainLayout.getBoardLayout().getHeight();
+        
         if (!hidden) {
-            Main.getDesign().baseRect(x, y, width, height);
-            Main.getDesign().text("Well done!", x, y, width, height);
+            PGraphics pg = Main.getInstance().getGraphics();
+            Main.getDesign().baseRect(
+                r.x + 3 * r.width / 8,
+                r.y + 3 * r.height / 8,
+                r.width / 4,
+                r.height / 8
+            );
+            
+            Main.getDesign().text(
+                "Well done!",
+                r.x + 3 * r.width / 8,
+                r.y + 3 * r.height / 8,
+                r.width / 4,
+                r.height / 8
+            );
         }
     }
     
     @Override
     public String mouseInput(int keyCode, float x, float y) {
+        Main.getInstance().getMainLayout().getBoardLayout().newRandomBoard();
+        
         hidden = !hidden;
         if (hidden) {
             Main.getInstance().resignClickable(this);
         }
         return "";
+    }
+    
+    public void unhide() {
+        hidden = false;
+        Main.getInstance().applyClickable(this);
     }
 }
