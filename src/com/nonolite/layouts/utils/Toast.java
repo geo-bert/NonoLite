58a5b4e9a3ba6a1db0733fc81d9d350b985b0166
duuwhang <maxipulstorm@aon.at>
2020-11-27 10:54:51 +0100

    cleanup Toast.java

--- src/com/nonolite/layouts/utils/Toast.java
+++ src/com/nonolite/layouts/utils/Toast.java
@@ -5,57 +5,46 @@ import com.nonolite.layouts.MainLayout;
 import processing.core.PGraphics;
 
 public class Toast extends Layout {
-    boolean _hidden;
+    boolean _hidden = true;
     String _text = "";
     
     public Toast(PGraphics pg) {
         super(pg);
-        _hidden = true;
     }
     
     @Override
     public void onLayout(float x, float y, float width, float height) {
         MainLayout mainLayout = Main.getInstance().getMainLayout();
-        Rect r = new Rect();
-        r.x = mainLayout.getBoardLayout().getX();
-        r.y = mainLayout.getBoardLayout().getY();
-        r.width = mainLayout.getBoardLayout().getWidth();
-        r.height = mainLayout.getBoardLayout().getHeight();
+        Rect rect = new Rect();
+        rect.x = mainLayout.getBoardLayout().getX();
+        rect.y = mainLayout.getBoardLayout().getY();
+        rect.width = mainLayout.getBoardLayout().getWidth();
+        rect.height = mainLayout.getBoardLayout().getHeight();
         
         if (!_hidden) {
-            PGraphics pg = Main.getInstance().getGraphics();
-            pg.push();
-            pg.fill(0, 150);
-            pg.rect(r.x, r.y, r.width, r.height);
-            pg.pop();
+            _pg.push();
+            _pg.fill(0, 150);
+            _pg.rect(rect.x, rect.y, rect.width, rect.height);
+            _pg.pop();
             
-            Main.getDesign().baseRect(
-                r.x + 3 * r.width / 8,
-                r.y + 3 * r.height / 8,
-                r.width / 4,
-                r.height / 8
-            );
+            Rect messageRect = new Rect();
+            messageRect.x = rect.x + 3 * rect.width / 8;
+            messageRect.y = rect.y + 3 * rect.height / 8;
+            messageRect.width = rect.width / 4;
+            messageRect.height = rect.height / 8;
             
-            Main.getDesign().text(
-                _text,
-                r.x + 3 * r.width / 8,
-                r.y + 3 * r.height / 8,
-                r.width / 4,
-                r.height / 8
-            );
+            Main.getDesign().baseRect(messageRect.x, messageRect.y, messageRect.width, messageRect.height);
+            Main.getDesign().text(_text, messageRect.x, messageRect.y, messageRect.width, messageRect.height);
         }
     }
     
     @Override
     public String mouseInput(int keyCode, float x, float y) {
         Main.getInstance().getMainLayout().getBoardLayout().newRandomBoard();
-        Main.getInstance().getMainLayout().getSideBarLayout().getTimerLayout().resetTimer();
         Main.getInstance().getMainLayout().getSideBarLayout().getTimerLayout().startTimer();
         
-        _hidden = !_hidden;
-        if (_hidden) {
-            Main.getInstance().resignClickable(this);
-        }
+        _hidden = true;
+        Main.getInstance().resignClickable(this);
         return "";
     }
     
