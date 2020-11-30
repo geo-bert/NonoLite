43f9574444dce3972b270c8dcafa02063d34acb5
duuwhang <maxipulstorm@aon.at>
2020-11-30 20:54:54 +0100

    add Rect QOL functionality

--- src/com/nonolite/layouts/utils/Rect.java
+++ src/com/nonolite/layouts/utils/Rect.java
@@ -5,4 +5,18 @@ public class Rect {
     public float y = 0;
     public float width = 0;
     public float height = 0;
+    
+    public Rect() {
+    }
+    
+    public Rect(float x, float y, float width, float height) {
+        set(x, y, width, height);
+    }
+    
+    public void set(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 }
